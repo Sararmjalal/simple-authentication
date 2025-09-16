a22468f1bdad1ac44e19a21ec13e076e5d96f165
Sararmjalal <sara.jalal.115@gmail.com>
2025-09-16 19:31:58 +0330

    feat: metadata

---
+++ lib/constants/metadata.ts
@@ -0,0 +1,9 @@
+import { ROUTES } from "@/types/enums/routes";
+
+export const meta = {
+  [ROUTES.HOME]: "Home",
+  [ROUTES.AUTH]: "Login / Sign up",
+  [ROUTES.DASHBOARD]: "Dashboard",
+  siteTitle: "Dekamond Test",
+  description: "Next.js app providing a simple authentication cycle"
+}

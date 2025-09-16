270e74a3dc98c32471e012606ab781d1ec8a2899
Sararmjalal <sara.jalal.115@gmail.com>
2025-09-16 21:41:54 +0330

    feat: auth + dashboard loading

---
+++ components/auth/Loading.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "../ui"
+
+const AuthLoading = () => {
+  return (
+    <Skeleton className="w-full h-[212px] md:max-w-md" />
+  )
+}
+
+export default AuthLoading
---
+++ components/dashboard/Loading.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "../ui"
+
+const DashboardLoading = () => {
+  return (
+    <Skeleton className="w-full h-[168px] md:max-w-md" />
+  )
+}
+
+export default DashboardLoading
